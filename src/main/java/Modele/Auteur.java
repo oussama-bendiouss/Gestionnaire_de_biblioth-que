026f03333672f6aac0eeb14b9c3a12fe3df5b650
unknown <bendioussoussama@gmail.com>
2021-10-27 15:49:20 +0200

    ajouter la class auteur

--- src/main/java/Modele/Auteur.java
+++ src/main/java/Modele/Auteur.java
@@ -1,4 +1,46 @@
 package Modele;
 
+
+
 public class Auteur {
+    public String[] nom;
+    public String[] prenom;
+   public String[] pseudo;
+   public String ID;
+
+
+    public String[] getNom() {
+        return this.nom;
+    }
+    public String[] getPrenom(){
+        return this.prenom;
+    }
+    public String[] getPseudo(){
+        return this.pseudo;
+    }
+    public String getID(){
+        return this.ID;
+    }
+
+    public void setNom(String[] nom) {
+        this.nom = nom;
+    }
+    public void setPrenom(String[] prenom){
+        this.prenom= prenom;
+    }
+    public void setPseudo(String[] pseudo){
+        this.pseudo= pseudo;
+    }
+
+
+    public  Auteur(String[] n, String[] p, String[] pseudo){
+        this.nom = n;
+        this.prenom = p;
+        this.pseudo= pseudo;
+        this.ID= n[0]+p[0]+pseudo;
+
+    }
+
+
+
 }
