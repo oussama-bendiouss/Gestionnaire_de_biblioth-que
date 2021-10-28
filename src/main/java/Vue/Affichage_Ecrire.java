f0fc6683da09fb46139e918b874fed83cc2097a0
unknown <bendioussoussama@gmail.com>
2021-10-28 12:50:40 +0200

    affichage de Ecrire

--- src/main/java/Vue/Affichage_Ecrire.java
+++ src/main/java/Vue/Affichage_Ecrire.java
@@ -1,6 +1,6 @@
 package Vue;
 
-import Modele.Oeuvre;
+import Modele.Ecrire;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -14,7 +14,7 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import static Controleur.GestionBD_SELECT.select_Oeuvre;
+import static Controleur.GestionBD_SELECT.select_Ecrire;
 
 public class Affichage_Ecrire extends Application   {
     @Override
@@ -25,21 +25,20 @@ public class Affichage_Ecrire extends Application   {
         HBox topControls = new HBox();
         VBox.setMargin( topControls, new Insets(10.0d) );
         topControls.setAlignment( Pos.BOTTOM_LEFT );
-        TableView<Oeuvre> tblCustomers = new TableView<>();
+        TableView<Ecrire> tblCustomers = new TableView<>();
         tblCustomers.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         VBox.setMargin( tblCustomers, new Insets(0.0d, 10.0d, 10.0d, 10.0d) );
         VBox.setVgrow( tblCustomers, Priority.ALWAYS );
 
-        TableColumn<Oeuvre, String> Titre = new TableColumn<>("Titre");
-        Titre.setCellValueFactory(new PropertyValueFactory<>("titre"));
+        TableColumn<Ecrire, String> ID_Auteur = new TableColumn<>("ID_Auteur");
+        ID_Auteur.setCellValueFactory(new PropertyValueFactory<>("ID_Auteur"));
 
-        TableColumn<Oeuvre, String> Description = new TableColumn<>("Description");
-        Description.setCellValueFactory(new PropertyValueFactory<>("description"));
+        TableColumn<Ecrire, String> ID_Oeuvre = new TableColumn<>("ID_Oeuvre");
+        ID_Oeuvre.setCellValueFactory(new PropertyValueFactory<>("ID_Oeuvre"));
 
-        TableColumn<Oeuvre, String> Année = new TableColumn<>("Année");
-        Année.setCellValueFactory(new PropertyValueFactory<>("Année"));
 
-        tblCustomers.getColumns().addAll( Titre, Description, Année );
+
+        tblCustomers.getColumns().addAll( ID_Auteur, ID_Oeuvre );
         Button btnRefresh = new Button("Refresh");
         btnRefresh.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -92,9 +91,9 @@ public class Affichage_Ecrire extends Application   {
 
 
 
-   private void loadTable(TableView<Oeuvre> tblCustomers) {
-       for (int i = 0; i < select_Oeuvre().size(); i++) {
-           tblCustomers.getItems().add(select_Oeuvre().get(i));
+   private void loadTable(TableView<Ecrire> tblCustomers) {
+       for (int i = 0; i < select_Ecrire().size(); i++) {
+           tblCustomers.getItems().add(select_Ecrire().get(i));
        }
 
        // tblCustomers.getItems().add(new Oeuvre("Hola", "Washington fff","2020-10-21"));
